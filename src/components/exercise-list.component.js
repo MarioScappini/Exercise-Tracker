9cce50609d9ee7f1e35a768215794464bfc55cb1
Mario Scappini <mario.scappiniu@gmail.com>
2020-07-20 13:17:52 -0400

    Changes

--- src/components/exercise-list.component.js
+++ src/components/exercise-list.component.js
@@ -1,6 +1,33 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default class ExerciseList extends Component{
+    constructor(props){
+        super(props);
+        
+        this.deleteExercises = this.deleteExercises.bind(this);
+        this.state = {exercises: []};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:5000/exercises/')
+            .then(response => {
+                this.setState({ exercises: response.data});
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    deleteExercises(id) {
+        axios.delete('http://localhost:5000/exercises/'+id)
+            .then(res => console.log(res.data));
+            this.setState({
+                
+            });
+    }
+
     render(){
         return(
             <div>
